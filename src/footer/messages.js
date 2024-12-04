b573134746280cb8ffddcfaa2b71d10ab3ec499f
Dawid Leśniak <dawid.lesniak@emag.lukasiewicz.gov.pl>
2024-12-04 10:23:36 +0100

    Change messages description

--- src/footer/messages.js
+++ src/footer/messages.js
@@ -4,22 +4,22 @@ const messages = defineMessages({
   'account.footer.privacyPolicyLinkLabel': {
     id: 'account.footer.privacyPolicyLinkLabel',
     defaultMessage: 'Privacy Policy',
-    description: 'Title tag',
+    description: 'The label for the link to the edX privacy policy page',
   },
   'account.footer.termsOfServiceLinkLabel': {
     id: 'account.footer.termsOfServiceLinkLabel',
     defaultMessage: 'Terms of Service',
-    description: 'Title tag',
+    description: 'The label for the link to the edX terms of service page',
   },
   'account.footer.accessibilityLinkLabel': {
     id: 'account.footer.accessibilityLinkLabel',
     defaultMessage: 'Accessibility',
-    description: 'Title tag',
+    description: 'The label for the link to the edX accessibility policy page',
   },
   'account.footer.helpLinkLabel': {
     id: 'account.footer.helpLinkLabel',
     defaultMessage: 'Help',
-    description: 'Title tag',
+    description: 'The label for the link to the edX help center',
   },
 });
 
